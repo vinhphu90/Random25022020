cacebd2019bcd0f6c55d6fe902be68858762cd57
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-03-18 22:44:05 +0700

    ui ap

--- app/src/main/java/com/examplevinhphutvp/random25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/random25022020/MainActivity.java
@@ -3,12 +3,18 @@ package com.examplevinhphutvp.random25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
